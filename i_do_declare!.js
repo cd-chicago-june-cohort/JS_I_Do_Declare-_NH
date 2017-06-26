ac7d9d739d799a69cf24fba996d708f77f1415c6
njharper26 <njharper@gmail.com>
2017-06-26 13:56:31 -0500

    initial commit

---
+++ i_do_declare!.js
@@ -0,0 +1,25 @@
+function a() 
+
+{
+var num1 = 100;
+var num2 = 2.0675;
+var num3 = 2000.64;
+var num4 = .32845;
+var string1 = 'Coding';
+var string2 = 'Dojo';
+var string3 = 'CodingDojo';
+var condTrue = true;
+var condFalse = false;  
+
+console.log('First Number', num1);
+console.log('Second Number', num2);
+console.log('Third Number', num3);
+console.log('4th Number', num4);
+console.log('First String', string1);
+console.log('2nd String', string2);
+console.log('String Number Three',string3)
+console.log('If conditional is TRUE', condTrue);
+console.log('If conditional is FALSE', condFalse);
+}
+
+a();
